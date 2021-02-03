d5cb0844cb4ff1304960612f7eb8dc0e1c0262b8
Johann Taylor <j.taylor1343@gmail.com>
2021-02-03 14:54:42 -0500

    Prompt input using inquire

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(userName, github);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const [userName, github] = profileDataArgs;
+// fs.writeFile('index.html', generatePage(userName, github), err => {
+//     if(err) throw err;
+//     console.log('Portfolio comlete! Check the index.html to see the output');
+// });
 
-fs.writeFile('index.html', generatePage(userName, github), err => {
-    if(err) throw err;
-    console.log('Portfolio comlete! Check the index.html to see the output');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
