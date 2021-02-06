605f9a2d4c5a18c103a125dd5fe97066d6a34fab
Johann Taylor <j.taylor1343@gmail.com>
2021-02-06 00:40:50 -0500

    Refactored code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(userName, github);
 
@@ -48,7 +48,9 @@ const promptUser = () => {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself',
-            when: ({ confirmAbout }) => {
+            when: ({
+                confirmAbout
+            }) => {
                 if (confirmAbout) {
                     return true;
                 } else {
@@ -70,77 +72,82 @@ const promptProject = portfolioData => {
 Add a New Project
 =================
     `);
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your project? (Required)',
-            validate: projectNameInput => { //validate method receives an argument
-                if (projectNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the name of your project!');
-                    return false;
+    return inquirer.prompt([{
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your project? (Required)',
+                validate: projectNameInput => { //validate method receives an argument
+                    if (projectNameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the name of your project!');
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of your project (Required)',
-            validate: descriptionInput => { //validate method receives an argument
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please add a description!');
-                    return false;
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide a description of your project (Required)',
+                validate: descriptionInput => { //validate method receives an argument
+                    if (descriptionInput) {
+                        return true;
+                    } else {
+                        console.log('Please add a description!');
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you build this project with? (Check all that apply).',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-        },
-        {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)',
-            validate: linkInput => { //validate method receives an argument
-                if (linkInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a link to your project!');
-                    return false;
+            },
+            {
+                type: 'checkbox',
+                name: 'languages',
+                message: 'What did you build this project with? (Check all that apply).',
+                choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+            },
+            {
+                type: 'input',
+                name: 'link',
+                message: 'Enter the GitHub link to your project. (Required)',
+                validate: linkInput => { //validate method receives an argument
+                    if (linkInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a link to your project!');
+                        return false;
+                    }
                 }
+            },
+            {
+                type: 'confirm',
+                name: 'feature',
+                message: 'Would you like to feature this project?',
+                default: false
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddProject',
+                message: 'Would you like to enter another project?',
+                default: false
             }
-        },
-        {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddProject',
-            message: 'Would you like to enter another project?',
-            default: false
-        }
-    ])
-    .then(projectData => {
-        portfolioData.projects.push(projectData); //pushes projectData from inquirer into the portfolioData.projects array
-        if (projectData.confirmAddProject) { //response is captured in answer object 'projectData' and verified by 'confirmAddProject' and checks to see if user wants to add another project
-            return promptProject(portfolioData); //if user chooses to add another project, then call promptProject function. If portfolioData isn't included as an argument, a new 'projects' array will be initialized and the existing project data will be lost
-        } else {
-            return portfolioData; //if add another project is false, then return the object and display the stored data
-        }
-    });
+        ])
+        .then(projectData => {
+            portfolioData.projects.push(projectData); //pushes projectData from inquirer into the portfolioData.projects array
+            if (projectData.confirmAddProject) { //response is captured in answer object 'projectData' and verified by 'confirmAddProject' and checks to see if user wants to add another project
+                return promptProject(portfolioData); //if user chooses to add another project, then call promptProject function. If portfolioData isn't included as an argument, a new 'projects' array will be initialized and the existing project data will be lost
+            } else {
+                return portfolioData; //if add another project is false, then return the object and display the stored data
+            }
+        });
 };
 
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+
+        fs.writeFile('index.html', generatePage(portfolioData), err => {
+            if (err) throw err;
+            console.log('Portfolio comlete! Check the index.html to see the output');
+        });
     });
